de8431f752e9d6a94803ad751b127f6064849c92
neeti-s <123490539+neeti-s@users.noreply.github.com>
2023-05-02 08:24:12 -0400

    Initial Commit

---
+++ WorryCalendar/WorryCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorryCalendarApp.swift
+//  WorryCalendar
+//
+//  Created by itpstudent on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WorryCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
